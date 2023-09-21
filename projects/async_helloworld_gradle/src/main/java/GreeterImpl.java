e75d1b05b501e67afb9ee81216e12886edc5c706
Hyuck Han <hyuck.han@furiosa.ai>
2023-09-21 12:51:20 +0900

    update impl code

--- projects/async_helloworld_gradle/src/main/java/GreeterImpl.java
+++ projects/async_helloworld_gradle/src/main/java/GreeterImpl.java
@@ -12,7 +12,28 @@ public class GreeterImpl extends GreeterGrpc.GreeterImplBase {
 
     @Override
     public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+        Thread worker = new HelloWorker(req, responseObserver);
+        worker.start();
+    }
+}
+
+class HelloWorker extends Thread {
+    private HelloRequest request;
+    private StreamObserver<HelloReply> responseObserver;
+
+    public HelloWorker(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        this.request = request;
+        this.responseObserver = responseObserver;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.currentThread().sleep(5000);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
     }
